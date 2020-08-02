04df473272f86e2c269d3434b251962cc1900c97
JustDenis <denisdenis2343@gmail.com>
2020-08-02 13:33:25 +0300

    Updates

---
+++ precache-manifest.633c25dbe35afe68a7ac46556da7e509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1942162b6c3db8690801bd9a27e0ba04",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "92be70fd94a7ed5ddcd5",
+    "url": "/goit-react-hw-01-components/static/css/main.898129b2.chunk.css"
+  },
+  {
+    "revision": "391bf100e355cb59e8eb",
+    "url": "/goit-react-hw-01-components/static/js/2.bff37411.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.bff37411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92be70fd94a7ed5ddcd5",
+    "url": "/goit-react-hw-01-components/static/js/main.344d6694.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
